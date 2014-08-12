9985386678e6f53e5e515efe5dfdccacf84ddfc7
Mat Ryer <mat@stretchr.com>
2014-08-11 19:14:38 -0600

    tweak docs

--- doc.go
+++ doc.go
@@ -1,7 +1,12 @@
 // Package slog provides concurrent levelled logging capabilities
 // with parent/child loggers.
 //
-// Example
+// * Concurrent safe
+// * Three levels; `slog.Err`, `slog.Warn`, and `slog.Info`
+// * Children loggers for sub-processes
+// * Built-in zero-memory `slog.NilLogger` to easily logging off without changing calling code
+// * Custom reporters to send logs anywhere
+// * `Reporters` function for reporting to many places
 //
 //     // make a logger to output to stdout
 //     l := slog.New("parent", slog.Warn)
